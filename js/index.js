0c53e37b47b06d80c046a307c95c18044188ae87
izabelasepiol <izabelasepiol@gmail.com>
2019-10-08 20:06:24 +0200

    initial commit

---
+++ js/index.js
@@ -0,0 +1,77 @@
+// // alert("Hello world");
+// var myName = "Marcin";
+// var familyName = "Dłubis";
+// var myMail = "frontendmarcin@gmail.com";
+// var liczbaUderzen;
+// var age = 30;
+
+// age = 31;
+// console.log("Mój wiek to " + age + "lat");
+
+// var number1 = 2;
+// var number2 = 4;
+// var result = number1 + number2;
+// console.log ("Wynik to liczba " + result);
+
+// var number3 = 5;
+// var number4 = 2;
+// var result = number3 % number4;
+// //konkatelacja// console.log ("Wynik z dzielenia " + result);
+
+// var number1 = number1 + 1;
+// console.log("number1 ma wartość: " +number1);
+// console.log("po zmienieniu number1 result wynosi: " + result);
+
+// var newNumber = 200;
+// if(newNumber==20)
+// {// console.log("Działa")// }
+// else if(newNumber > 200)
+// {console.log("Liczba jest wieksza lub równa 200")}
+
+// else if (newNumber < 200)
+// {console.log("Liczba jest mniejsza lub równa 200")}
+// else
+// {// console.log("To nie ta liczba")// };
+
+// var brandNewNumber = 200;
+// var completelyBrandNewNumber = 900;
+// if ((completelyBrandNewNumber < 100) && (brandNewNumber > 600))
+// {console.log ("Mieści się w zakresie")}
+// else
+// {console.log ("Nie mieści się w zakresie")};
+
+// var wiek;
+// wiek= prompt ("Podaj swój wiek");
+// {console.log(wiek)};// if (isNaN(wiek))
+// {console.log ("podaj swój wiek za pomocą liczby")}
+
+// else if (wiek >= 18)
+// {console.log("OK")}
+// else {console.log("Ta strona nie jest dla ciebie")};
+
+
+// pętle 
+//
+// var number = 0;
+// number = number +20
+// var number = 5
+// 
+// while (number <=10)
+// 
+// {console.log (number);// // number++;// // }
+// // do {console.log (number);// // number++// // } while (number > 10);
+
+// for(var i=0; i<=10;i++)// {console.log(i)}
+// for(var i=1; i<=100;i++)// {console.log(i)}
+// for(var i=5; i<=50;i++)// {console.log(i)}
+
+// for(var i=1; i<=100;i++)// {// if (i%8);// var result=(i%8);// console.log (i++);// } tu jest cos zle
+//var zmienna1 = "Marcin";function nazwaFunkcji() {//console.log("Funkcja coś wyświetla");}return "Witaj";}nazwaFunkcji();
+//var zmienna = nazwaFunkcji();console.log
+
+var number=prompt("Podaj liczbę");
+
+if(number%2)
+{document.write(""+number+" jest liczbą nieparzystą");}
+else
+{document.write(""+number+" jest liczbą parzystą")};
